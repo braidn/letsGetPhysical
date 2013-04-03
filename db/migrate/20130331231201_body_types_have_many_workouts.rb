e6bef925ad1ce22d6d96f09290099513c73fd749
Braden <braden.douglass@gmail.com>
2013-04-03 13:57:13 -0400

    add an index on the body_types in specific workouts

--- db/migrate/20130331231201_body_types_have_many_workouts.rb
+++ db/migrate/20130331231201_body_types_have_many_workouts.rb
@@ -2,12 +2,14 @@ class BodyTypesHaveManyWorkouts < ActiveRecord::Migration
   def up
     change_table :workouts do |t|
       t.references :body_types
+      t.index :body_type
     end
   end
 
   def down
     change_table :workouts do |t|
       t.remove_references :body_types
+      t.remove_index :body_type
     end
   end
 end
