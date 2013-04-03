beac910f616ef7dcfa6316be054744003b6e4e09
Braden <braden.douglass@gmail.com>
2013-04-02 22:05:06 -0400

    body_types controller and views. scaffold_controller used and controller cleaned up. Who knows what to do with dem views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 WomenWorkout::Application.routes.draw do
   resources :users
   resources :sessions
+  resources :body_types
   root :to => 'home#index'
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
