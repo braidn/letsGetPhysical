74437d42194f57449c6b8e1068a2c3c542a58ee9
Braden <braden.douglass@gmail.com>
2013-04-02 21:06:10 -0400

    secret item added to make sure things were working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+  before_filter :require_login, :only => :secret
+
   def index
     # nothing, pure html baby... for now
+  end
+
+  def secret
+    # not so, huh
   end
 end
--- app/models/body_type.rb
+++ app/models/body_type.rb
@@ -1,5 +1,5 @@
 class BodyType < ActiveRecord::Base
   attr_accessible :description, :title
-
+  has_many :workouts
   validates :description, :title, :presence => true
 end
