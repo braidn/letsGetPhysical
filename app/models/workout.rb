cf8505a6d96209c9d8c53bad59278cc437bebf69
Braden <braden.douglass@gmail.com>
2013-07-28 16:30:40 -0400

    Changed the body_type array to better correspond with where Kyle was going with the library.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,6 @@
 class Workout < ActiveRecord::Base
 	has_and_belongs_to_many :combos
-  BODY_TYPES = %w{abs back legs arms cardio}
+  BODY_TYPES = %w{abs shoulders legs arms plyos}
   validates :body_type, :inclusion => {:in => BODY_TYPES}
   attr_accessible :description, :image, :title, :video, :body_type
 end
