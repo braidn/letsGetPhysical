54d25c91b3749b3c009e9e07d5e7cf1de93bb667
Braden <braden.douglass@gmail.com>
2013-07-28 18:19:14 -0400

    Workout model simply flushed out in a better than nothing test

--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -1,7 +1,20 @@
 require 'spec_helper'
 
 describe Workout do
-	it 'should have a valid factory' do
-		Fabricate(:workout).should be_valid
+  let(:workout) { Fabricate(:workout) }
+
+	it 'has a valid factory' do
+		workout.should be_valid
 	end
+
+  context 'with an invalid body_type' do
+    it 'fails when saving' do
+      expect{ Fabricate(:workout, body_type: 'flabs') }.to raise_error
+    end
+  end
+  context 'with a valid body_type' do
+    it 'saves spectacularly' do
+      expect{ Fabricate(:workout, body_types: 'shoulders') }.to be_true
+    end
+  end
 end
